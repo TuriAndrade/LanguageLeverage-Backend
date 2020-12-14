e1ae08440b82c64ab1f18ca8ebdc32bed999dc62
Turi Vasconcelos <turivasconcelos@gmail.com>
2020-12-13 22:48:21 -0300

    Redis auth

--- src/database/sessions-db.js
+++ src/database/sessions-db.js
@@ -1,6 +1,9 @@
 const redis = require("redis");
 
-const sessionsDb = redis.createClient();
+const sessionsDb = redis.createClient({
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+});
 
 sessionsDb.on("error", (error) => {
   console.error(error);
